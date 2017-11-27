2c7d4fbffd21b408374839e945c7675d45a6dd48
Vinay Punwani <vinay30@users.noreply.github.com>
2017-11-27 02:06:01 -0800

    [Portfolio] Update to lightbox, styling, structure

---
+++ src/lightbox.js
@@ -0,0 +1,42 @@
+var slideIndex = 1;
+var activeModalName = 'myModal';
+
+function openModal(modalName) {
+  document.getElementById(modalName).style.display = "block";
+  activeModalName = modalName;
+}
+
+function closeModal(modalName) {
+  document.getElementById(modalName).style.display = "none";
+  activeModalName = null;
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slidesSelector = "#" + activeModalName + " .mySlides";
+  var dotsSelector = "#" + activeModalName + " .demo";
+  var slides = document.querySelectorAll(slidesSelector);
+  var dots = document.querySelectorAll(dotsSelector);
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+showSlides(slideIndex);
